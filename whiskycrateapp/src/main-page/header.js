eb68983f9f40e986664725ede039bb2005265d56
Ashley Kilgour <kilgourkilgour@hotmail.com>
2021-07-29 15:20:50 +0100

    Added a header

---
+++ whiskycrateapp/src/main-page/header.js
@@ -0,0 +1,14 @@
+import logo   from "./whisky-logo.jpg"
+
+const Header = () => (
+  <header className="row">
+    <div className="col-md-5">
+      <img src={logo} className="logo" alt="logo" />
+    </div>
+    <div className="col-md-7 mt-5 subtitle">
+      Reviewing whisky from all over the world
+    </div>
+  </header>
+);
+
+export default Header;
